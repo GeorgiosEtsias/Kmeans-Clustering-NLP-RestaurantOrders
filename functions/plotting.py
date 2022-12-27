187cf489d84060fe10d7e34fb5e63449c114f3e7
Georgios Etsias <g.etsias@qub.ac.uk>
2022-12-27 20:28:11 +0200

    update functions/plotting.py  add export fig

--- functions/plotting.py
+++ functions/plotting.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.io as pio
+import os
 
-def plot_cluster_pie_chart(clusternames, product_distribution, plot_title):
+def matplotlib_pie_chart(clusternames, product_distribution, plot_title, export_graph):
     '''
     Creates a pie chart to show the distribution of the various products
     using matplotlib
@@ -11,13 +13,54 @@ def plot_cluster_pie_chart(clusternames, product_distribution, plot_title):
     ax.axis('equal')  
     ax.set_title(plot_title)
     plt.show()
+
+    if export_graph:
+        # export png
+        export_png(fig, plot_title)  
+
     return fig
 
-def plot_cluster_pie_chart2(clusternames, product_distribution, plot_title):
+def plotly_pie_chart(clusternames, product_distribution, plot_title, export_graph):
     '''
     Creates a pie chart to show the distribution of the various products using Plotly
     '''
-    # fig = px.pie(values=product_distribution, labels=clusternames, title=plot_title)
     fig = px.pie(values=product_distribution, names=clusternames, title=plot_title)
     fig.show()
+    
+    if export_graph:
+        # export html
+        export_html(fig, plot_title)  
+    
     return fig
+
+def export_png(fig, plot_title):
+    '''
+    Save the matplotlib figure as a png
+    '''
+    directory = 'png_figures/'
+    # check if the directory exists, if not create it
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    figure_name = str(directory) + plot_title + '.png'   
+    fig.savefig(figure_name, dpi=300)
+
+def export_html(fig, plot_title):
+    '''
+    Save the figure plotly as an interactive image file (html)
+    '''
+    directory = 'html_figures/'
+    # check if the directory exists, if not create it
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    figure_name = str(directory) + plot_title + '.html'   
+    pio.write_html(fig, figure_name)
+
+
+
+
+directory = '/path/to/my/directory'
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
